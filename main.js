aabe0b89b976ce6116bdaf93160f04b3a573518a
GurnikVitaliy <111244081+GurnikVitaliy@users.noreply.github.com>
2023-07-18 00:12:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -22,26 +22,26 @@ btn1.addEventListener('click', () => {
     xttp.send();
    },4000)
 })
-// const print = document.querySelector('.print');
+const print = document.querySelector('.print');
 
-// const btn2 = document.querySelector('#btn2');
-// const token2 = '6050757748:AAE0SlqzlC9im6u5LJpO0k30O4xxT8WLeT8';
+const btn2 = document.querySelector('#btn2');
+const token2 = '6050757748:AAE0SlqzlC9im6u5LJpO0k30O4xxT8WLeT8';
 
-// btn2.addEventListener('click', () => {
-//     setTimeout(() => {
-//      const input1 = document.querySelector('#input1');
-//      let inp1 = input1.value;
+btn2.addEventListener('click', () => {
+    setTimeout(() => {
+     const input1 = document.querySelector('#input1');
+     let inp1 = input1.value;
  
-//      input1.value = '';
+     input1.value = '';
  
-//       const url = 'https://api.telegram.org/bot'+token1+'/sendMessage?chat_id=1201316750&text=';
-//      let xttp = new XMLHttpRequest();
+      const url = 'https://api.telegram.org/bot'+token1+'/sendMessage?chat_id=1201316750&text=';
+     let xttp = new XMLHttpRequest();
  
-//      xttp.open("GET", url+inp1);
-//      xttp.send();
-//     },5000)
-//  })
-//  const printt = document.querySelector('.print');
+     xttp.open("GET", url+inp1);
+     xttp.send();
+    },5000)
+ })
+ const printt = document.querySelector('.print');
 
 
 
